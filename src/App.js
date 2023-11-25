776974fc3230096a43f83fa025c37ef262bbf3c7
KoolTheba <gomez.teba@gmail.com>
2023-11-25 17:21:24 +0100

    feat: added websockets on App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080");
+
+    const apiCall = {
+      event: "content_generator:request",
+      data: { description: "Hello", urls: ["www.hello.com/123456"] },
+    };
+
+    ws.onopen = (event) => {
+      ws.send(JSON.stringify(apiCall));
+    };
+
+    ws.onmessage = function (ev) {
+      const { data } = JSON.parse(ev.data);
+      setData(data);
+    };
+
+    return () => ws.close();
+  }, []);
+
+  if (!data) {
+    return <p>Loading....content coming soon</p>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img
+        src={data?.pictures[0].url}
+        alt={data?.pictures[0].description}
+        width="200px"
+      />
+      <p> {data?.postDescription}</p>
     </div>
   );
 }
